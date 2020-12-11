d968f6f377ce8dcd9b89f14b661335027a390af5
Courtney Phillips <quirkyfiddie@yahoo.com>
2020-12-10 21:34:28 -0600

    added color and random planet

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
 
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
-   /*window.addEventListener("load", function() {
-            fetch("https://handlers.education.launchcode.org/static/weather.json").then( function(response) {
-               response.json().then( function(json) {
-                  const div = document.getElementById("weather-conditions");
-                  // Add HTML that includes the JSON data
-                  div.innerHTML = `
-                     <ul>
-                        <li>Temp ${json.temp}</li>
-                        <li>Wind Speed ${json.windSpeed}</li>
-                        <li>Status ${json.status}</li>
-                        <li>Chance of Precip ${json.chanceOfPrecipitation}</li>
-                     </ul>
-                  `;
-               });
-            });
-         });
-         */
    form.addEventListener("submit", function(event) {
       let pilotNameInput = document.getElementById("pilotName");
       let copilotNameInput = document.getElementById("copilotName");
@@ -26,13 +9,13 @@ window.addEventListener("load", function() {
       if (pilotNameInput.value === "" || typeof pilotNameInput.value !== "string") {
          alert("Pilot input must be a name!");
       }  else {
-         document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotNameInput.value} Ready`;
+         document.getElementById("pilotStatus").innerHTML = `Pilot "${pilotNameInput.value}" Ready`;
       }
       
       if (copilotNameInput.value === "" || typeof copilotNameInput.value !== "string") {
          alert("Co-Pilot input must be a name!");
       }  else {
-         document.getElementById("copilotStatus").innerHTML = `Co-Pilot ${copilotNameInput.value} Ready`;
+         document.getElementById("copilotStatus").innerHTML = `Co-Pilot "${copilotNameInput.value}" Ready`;
       }
 
       if (fuelLevelInput.value === "" || isNaN(Number(fuelLevelInput.value)) === true) {
@@ -42,37 +25,44 @@ window.addEventListener("load", function() {
          alert("Cargo Mass must be a number!");
       }      
       
-      if (fuelLevelInput.value < 10000) {
-            document.getElementById("faultyItems").style.visibility = "visible";
+      if (fuelLevelInput.value < 10000 || cargoMassInput.value > 10000) {
+         document.getElementById("faultyItems").style.visibility = "visible";
+         document.getElementById("launchStatus").innerHTML = `Shuttle not ready for launch`;
+         document.getElementById("launchStatus").style.color = "red";
+         if (fuelLevelInput.value < 10000) {
+            document.getElementById("fuelStatus").style.color = "red";
             document.getElementById("fuelStatus").innerHTML = `Fuel level of ${fuelLevelInput.value} is too low for launch!`;
-            document.getElementById("launchStatus").innerHTML = `Shuttle not ready for launch`;
-            document.getElementById("launchStatus").style.color = "red";
-            event.preventDefault();
-      }  else { 
+         }
+         event.preventDefault();
          if (cargoMassInput.value > 10000) {
-            document.getElementById("faultyItems").style.visibility = "visible";
+            document.getElementById("cargoStatus").style.color = "red";
             document.getElementById("cargoStatus").innerHTML = `Cargo mass of ${cargoMassInput.value} is too high for launch!`;
-            document.getElementById("launchStatus").innerHTML = `Shuttle not ready for launch`;
-            document.getElementById("launchStatus").style.color = "red"; 
-            event.preventDefault();
+         }
+         event.preventDefault();
+       
       }  else {      
          document.getElementById("launchStatus").innerHTML = `Shuttle is ready for launch!`;
          document.getElementById("launchStatus").style.color = "green";
       }
       event.preventDefault();
-      }
-   });
-   
+   })
 
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+      response.json().then( function(json) {
+         const planet = document.getElementById("missionTarget");
+         let index = Math.floor(Math.random()*json.length);
+         planet.innerHTML = `
+         <h2>Mission Destination:</h2>
+         <ol>
+            <li>Name: ${json[index].name}</li>
+            <li>Diameter: ${json[index].diameter}</li>
+            <li>Star: ${json[index].star}</li>
+            <li>Distance from Earth: ${json[index].distance}</li>
+            <li>Number of Moons: ${json[index].moons}</li>
+         </ol>
+         <img src="${json[index].image}">
+         `
+     });
+   })
 });
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+
